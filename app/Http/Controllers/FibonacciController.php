b9ab4ee81996d8efaefd80278ff1518ede2fa0f7
Maria Paula <paulapeme5@gmail.com>
2023-05-29 14:34:19 -0500

    Actualizado

--- app/Http/Controllers/FibonacciController.php
+++ app/Http/Controllers/FibonacciController.php
@@ -10,51 +10,50 @@ class FibonacciController extends Controller
         return view('fibonacci');
     }
 
-    public function index(Request $request){
-        $numero = $request->numero;
-        $resultado=[$this->resultado($numero)];
-        $resultado = implode(" , ",$resultado);
-        return view (('fibonacci'), compact('resultado'));
-    }
+    // public function index(Request $request){
+    //     $numero = $request->numero;
+    //     $resultado=[$this->resultado($numero)];
+    //     $resultado = implode(" , ",$resultado);
+    //     return view (('fibonacci'), compact('resultado'));
+    // }
 
-    public function fibonacci($numero){
-            if (($numero==0) or ($numero==1)) {
-                return $numero;
-            }else{
-                return $this->fibonacci($numero -1) + $this->fibonacci($numero -2);
-            }
-            }
-            public function resultado($numero){
-                for ($i=0; $i < $numero ; $i++) {
-                    echo $this->fibonacci($i);
-        }
-
-     }
-    }
+    // public function fibonacci($numero){
+    //         if (($numero==0) or ($numero==1)) {
+    //             return $numero;
+    //         }else{
+    //             return $this->fibonacci($numero -1) + $this->fibonacci($numero -2);
+    //         }
+    //         }
+    //         public function resultado($numero){
+    //             for ($i=0; $i < $numero ; $i++) {
+    //                 echo $this->fibonacci($i);
+    //     }
+    //  }
+    // }
 
     //SIN RECURSIVIDAD
 
-//     public function index(Request $request){
-//         $numero = $request->numero;
-//         $num1 = 0;
-//         $num2 = 1;
-//         $num3 = null;
-//         $resultado=[];
-//         for ($i=0; $i < $numero ; $i++){
-//         if($i==0){
-//             $resultado[] = $num1;
-//         }
-//         else if($i==1){
-//             $resultado[] = $num2;
-//         } else{
-//             $num3=$num1+$num2;
-//             $resultado[] = $num3;
-//             $num1=$num2;
-//             $num2=$num3;
-//         }
-//     }
-//         $resultado = implode(", ", $resultado);
-//         return view('fibonacci', compact('resultado'));
-//     }
+    public function index(Request $request){
+        $numero = $request->numero;
+        $num1 = 0;
+        $num2 = 1;
+        $num3 = null;
+        $resultado=[];
+        for ($i=0; $i < $numero ; $i++){
+        if($i==0){
+            $resultado[] = $num1;
+        }
+        else if($i==1){
+            $resultado[] = $num2;
+        } else{
+            $num3=$num1+$num2;
+            $resultado[] = $num3;
+            $num1=$num2;
+            $num2=$num3;
+        }
+    }
+        $resultado = implode(", ", $resultado);
+        return view('fibonacci', compact('resultado'));
+    }
 
-// }
+}
